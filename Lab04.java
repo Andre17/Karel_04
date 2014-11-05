f83076a46fb188253b7ae446d42b1f99b6899c20
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro-3.local>
2014-11-05 10:37:13 +0000

    Lab04

--- Lab04.java
+++ Lab04.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <André Purits>  // replace <...> with your name
+* @version <03/11.14> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
@@ -11,7 +11,64 @@ import edu.fcps.karel2.Robot;
 
 public class Lab04 {
 
+public static void takeTheField(Athlete arg)
+{
+   arg.move();
+   arg.move();
+   arg.move();
+   arg.move();
+   arg.turnRight();
+   arg.move();
+   arg.move();
+}
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/arena.map");
+     Display.setSize(10, 10);
+     Display.setSpeed(3);
+      Athlete andre = new Athlete();
+      Athlete giroud = new Athlete();
+      Athlete zlatan = new Athlete();
+      Athlete thierry = new Athlete();
+      Athlete olivier = new Athlete();
+      Athlete ibrahimovic = new Athlete();
+      takeTheField(andre);
+      takeTheField(giroud);
+      takeTheField(zlatan);
+      takeTheField(thierry);
+      takeTheField(olivier);
+      takeTheField(ibrahimovic);
+      andre.move();
+      andre.turnLeft();
+      andre.move();
+      andre.turnAround();
+      zlatan.move();
+      zlatan.move();
+      zlatan.move();
+      zlatan.turnLeft();
+      zlatan.move();
+      zlatan.move();
+      zlatan.turnAround();
+      giroud.move();
+      giroud.move();
+      giroud.move();
+      giroud.move();
+      giroud.move();
+      giroud.turnLeft();
+      giroud.move();
+      giroud.turnAround();
+      thierry.move();
+      thierry.move();
+      thierry.move();
+      thierry.move();
+      thierry.turnRight();
+      olivier.move();
+      olivier.move();
+      olivier.move();
+      olivier.turnRight();
+      ibrahimovic.move();
+      ibrahimovic.move();
+      ibrahimovic.turnRight();
+      Athlete coach = new Athlete(2, 7, Display.EAST, 0);
+
      }
 }
